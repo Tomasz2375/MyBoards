dad921c614b3a3a284d4d041ebbd0e9259f815c4
Tomasz <@gmial.com>
2023-01-26 14:21:39 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using MyBoards.Entities;
+
+namespace MyBoards
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<MyBoardsContext>(
+                option => option.UseSqlServer
+                (builder.Configuration.GetConnectionString("MyBoardsConnectionString"))
+                );
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.Run();
+        }
+    }
+}
